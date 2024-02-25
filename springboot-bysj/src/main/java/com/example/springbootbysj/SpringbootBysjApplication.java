1ae3392a6d7b4161f66754bfc9af0f6ed95e60e5
panliming <2194838813@qq.com>
2024-02-25 22:46:49 +0800

    feat:初始化

---
+++ springboot-bysj/src/main/java/com/example/springbootbysj/SpringbootBysjApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbysj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBysjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBysjApplication.class, args);
+    }
+
+}
